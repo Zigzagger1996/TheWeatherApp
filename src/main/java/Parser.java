a5ad970d1445b53bf4ab312f59dddc685a7b7a0e
Zigzagger1996 <vova.glukhomanyk@gmail.com>
2017-10-06 18:37:13 +0300

    Download information about date and temperature. Bug: temperature is load incorrect, in one day loads information about week temperature.

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -2,15 +2,32 @@ import java.io.IOException;
 import java.net.URL;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class Parser {
 
     private static Document getPage() throws IOException {
-        String url = "https://www.accuweather.com/ru/world-weather";
+        String url = "https://ua.sinoptik.ua/";
         Document page = Jsoup.parse(new URL(url), 3000);
         return page;
     }
     public static void main(String[] args) throws IOException {
-        System.out.println(getPage());
+        Document page = getPage();
+        //css query language
+        Element divWth = page.select("div[class = tabs]").first();
+        Element todayDate = divWth.select("div[class = main loader]").first();
+        Elements otherDayInWeekDate = divWth.select("div[class = main]");
+        Elements wthPerWeek = divWth.select("div[class = temperature]");
+
+        for (Element name : otherDayInWeekDate ) {
+            String dayFree = name.select("p[class=date dateFree]").text();
+            String dayWorking = name.select("p[class=date]").text();
+            String month = name.select("p[class=month]").text();
+            String minTemperature = wthPerWeek.select("div[class = min]").text();
+            String maxTemperature = wthPerWeek.select("div[class = max]").text();
+            System.out.println("Дата: " + dayWorking + dayFree + " месяц: " + month + " Температура: " + minTemperature + maxTemperature);
+        }
+        System.out.println("");
     }
 }
