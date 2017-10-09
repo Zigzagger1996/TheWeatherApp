84ab0d318db2d83942b68b47468b24da6ef2b3c5
Zigzagger1996 <vova.glukhomanyk@gmail.com>
2017-10-09 09:33:09 +0300

    Fix string name.

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -16,7 +16,7 @@ public class Controller {
 
     public void processUser() throws IOException{
         TextConstants textConstants = new TextConstants();
-        model.getTemperatureInfo(textConstants.URL_FOR_PARSE, textConstants.DIV_WTH_STRING, textConstants.DAY_FREE_STRING,textConstants.OTHER_DAY_IN_WEEK_STRING, textConstants.WTH_PER_WEEK_STRING, textConstants.DAY_FREE_STRING, textConstants.DAY_WORK_STRING, textConstants.MOUNTH_STRING, textConstants.MIN_TEMPERATURE_STRING, textConstants.MAX_TEMPERATURE_STRING);
+        model.getTemperatureInfo(textConstants.URL_FOR_PARSE, textConstants.DIV_WTH_STRING, textConstants.DAY_FREE_STRING,textConstants.OTHER_DAY_IN_WEEK_STRING, textConstants.WTH_PER_WEEK_STRING, textConstants.DAY_FREE_STRING, textConstants.DAY_WORK_STRING, textConstants.MONTH_STRING, textConstants.MIN_TEMPERATURE_STRING, textConstants.MAX_TEMPERATURE_STRING);
     }
 
 }
