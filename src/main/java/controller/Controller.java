0d55153b5cf315f0e548e22d5b0e00fd9320d6fe
Zigzagger1996 <vova.glukhomanyk@gmail.com>
2017-10-09 11:41:04 +0300

    make all constant string as public static final and delete textConstant object from controller

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -15,8 +15,7 @@ public class Controller {
     }
 
     public void processUser() throws IOException{
-        TextConstants textConstants = new TextConstants();
-        model.getTemperatureInfo(textConstants.URL_FOR_PARSE, textConstants.DIV_WTH_STRING, textConstants.DAY_FREE_STRING,textConstants.OTHER_DAY_IN_WEEK_STRING, textConstants.WTH_PER_WEEK_STRING, textConstants.DAY_FREE_STRING, textConstants.DAY_WORK_STRING, textConstants.MONTH_STRING, textConstants.MIN_TEMPERATURE_STRING, textConstants.MAX_TEMPERATURE_STRING);
+        model.getTemperatureInfo(TextConstants.URL_FOR_PARSE, TextConstants.DIV_WTH_STRING, TextConstants.DAY_FREE_STRING,TextConstants.OTHER_DAY_IN_WEEK_STRING, TextConstants.WTH_PER_WEEK_STRING, TextConstants.DAY_FREE_STRING, TextConstants.DAY_WORK_STRING, TextConstants.MONTH_STRING, TextConstants.MIN_TEMPERATURE_STRING, TextConstants.MAX_TEMPERATURE_STRING);
     }
 
 }
--- src/main/java/view/TextConstants.java
+++ src/main/java/view/TextConstants.java
@@ -1,14 +1,14 @@
 package view;
 
 public class TextConstants {
-    public String URL_FOR_PARSE = "https://ua.sinoptik.ua/";
-    public String DIV_WTH_STRING = "div[class = tabs]";
-    public String TODAY_DAY_STRING = "div[class = main loader]";
-    public String OTHER_DAY_IN_WEEK_STRING = "div[class = main]";
-    public String WTH_PER_WEEK_STRING = "div[class = temperature]";
-    public String DAY_FREE_STRING = "p[class=date dateFree]";
-    public String DAY_WORK_STRING = "p[class=date]";
-    public String MONTH_STRING = "p[class=month]";
-    public String MIN_TEMPERATURE_STRING = "div[class = min]";
-    public String MAX_TEMPERATURE_STRING = "div[class = max]";
+    public static final String URL_FOR_PARSE = "https://ua.sinoptik.ua/";
+    public static final String DIV_WTH_STRING = "div[class = tabs]";
+    public static final String TODAY_DAY_STRING = "div[class = main loader]";
+    public static final String OTHER_DAY_IN_WEEK_STRING = "div[class = main]";
+    public static final String WTH_PER_WEEK_STRING = "div[class = temperature]";
+    public static final String DAY_FREE_STRING = "p[class=date dateFree]";
+    public static final String DAY_WORK_STRING = "p[class=date]";
+    public static final String MONTH_STRING = "p[class=month]";
+    public static final String MIN_TEMPERATURE_STRING = "div[class = min]";
+    public static final String MAX_TEMPERATURE_STRING = "div[class = max]";
 }
